b857de1e04e04226672677cf9926ff238a538ebb
Markouka <mark.bestavros@live.com>
2015-01-31 14:30:08 -0500

    Start work on checking Google PLay Services

--- app/src/main/java/appathon/bu/com/appathon/MainActivity.java
+++ app/src/main/java/appathon/bu/com/appathon/MainActivity.java
@@ -2,6 +2,7 @@ package appathon.bu.com.appathon;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -14,6 +15,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 public class MainActivity extends Activity {
@@ -32,7 +35,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-                navDrawerTitles = getResources().getStringArray(R.array.nav_array);
+
+
+//        if(GooglePlayServicesUtil.isGooglePlayServicesAvailable(this)){
+//            GooglePlayServicesUtil.getErrorDialog()
+//        }
+
+
+        {
+            navDrawerTitles = getResources().getStringArray(R.array.nav_array);
+        }
         navDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
