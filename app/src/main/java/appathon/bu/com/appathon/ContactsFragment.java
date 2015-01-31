420d0310edc0c04636ed84d000313b1c9b38af34
Markouka <mark.bestavros@live.com>
2015-01-31 18:54:59 -0500

    Add list adapters to display contacts

--- app/src/main/java/appathon/bu/com/appathon/ContactsFragment.java
+++ app/src/main/java/appathon/bu/com/appathon/ContactsFragment.java
@@ -9,13 +9,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by FreddieV4 on 1/31/2015.
  */
 public class ContactsFragment extends Fragment {
 
     //private final static String[] FROM_COLUMNS = new String[];
-
+    public static String[] names;
+    public static String[] phoneNumbers;
+    ListView lv;
 
 
     @Override
@@ -23,25 +31,35 @@ public class ContactsFragment extends Fragment {
 
         // Inflating the layout
         View v = inflater.inflate(R.layout.contacts_view, container, false);
+        lv = (ListView)v.findViewById(R.id.listView);
 
 
-        readContacts();
+        String name = null;
+        String phoneNumber = null;
+
+        ContentResolver cr = this.getActivity().getContentResolver();
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        while (phones.moveToNext())
+        {
+            name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+        }
+        phones.close();
+
+        names = name.split("\\s+");
+        phoneNumbers = phoneNumber.split("\\s+");
+
+
+        ArrayAdapter<String> aA = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names);
+
+        for(int i = 0; i<names.length; i++){
+            lv.setAdapter(aA);
+        }
 
 
         return v;
     }
 
-    private void readContacts(){
-        ContentResolver cr = this.getActivity().getContentResolver();
-        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
-        while (phones.moveToNext())
-        {
-            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-        }
-        phones.close();
-
-    }
 
 }
