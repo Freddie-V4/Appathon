608a7a389868329c4286caeb722cb992549077ab
Freddie <fv4.programmer@gmail.com>
2015-03-10 10:06:24 -0400

    Center CardView in ActivityView class

--- app/src/main/java/appathon/bu/com/appathon/CardsActivity.java
+++ app/src/main/java/appathon/bu/com/appathon/CardsActivity.java
@@ -2,11 +2,12 @@ package appathon.bu.com.appathon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by FreddieV4 on 1/31/2015.
  */
-public class CardsActivity extends Activity {
+public class CardsActivity extends ActionBarActivity {
 
 
     @Override
