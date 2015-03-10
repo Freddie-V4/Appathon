4d9762151ab7adb608e046e44aa98e38ff5bfa63
Freddie <fv4.programmer@gmail.com>
2015-03-10 10:02:07 -0400

    Add Intent to ImageButton1 in ReasonsActivity; used to view CardsActivity while editing

--- app/src/main/java/appathon/bu/com/appathon/ReasonsActivity.java
+++ app/src/main/java/appathon/bu/com/appathon/ReasonsActivity.java
@@ -1,6 +1,7 @@
 package appathon.bu.com.appathon;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by FreddieV4 on 1/31/2015.
@@ -19,6 +21,22 @@ public class ReasonsActivity extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reasons_view);
+
+
+        setClicks();
+
+    }
+
+    private void setClicks(){
+        ImageButton ib = (ImageButton) findViewById(R.id.imageButton1);
+
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ReasonsActivity.this, CardsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
