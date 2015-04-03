3b12e89ffa418776ad2b746387d7a6ed8e0c2ff5
Freddie <fv4.programmer@gmail.com>
2015-04-02 22:58:05 -0400

    Refactor code & test custom button bg

--- app/src/main/java/appathon/bu/com/appathon/StartingActivity.java
+++ app/src/main/java/appathon/bu/com/appathon/StartingActivity.java
@@ -1,5 +1,6 @@
 package appathon.bu.com.appathon;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -16,7 +17,7 @@ import com.parse.ParseObject;
 /**
  * Created by FreddieV4 on 1/31/2015.
  */
-public class StartingActivity extends ActionBarActivity implements View.OnClickListener {
+public class StartingActivity extends Activity {
 
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private boolean enabled = true;
@@ -38,17 +39,29 @@ public class StartingActivity extends ActionBarActivity implements View.OnClickL
 
         Parse.initialize(this, "y3D6qePfthe7VxIIHdwynl7csbYGoDDOPFXteYHa", "l7QD57x45ILOGQidOtZq7DiD9tRmY3dVe115krz7");
 
+        // Find EditText views
+        name = (EditText) findViewById(R.id.nameText);
+        phoneNumber = (EditText) findViewById(R.id.phoneNumberText);
+
         enter = (Button) findViewById(R.id.enterButton);
-        enter.setOnClickListener(this);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get user input from EditText and add values to strings
+                nameString = name.getText().toString();
+                phoneString = phoneNumber.getText().toString();
 
-        android.support.v7.app.ActionBar ab = getSupportActionBar();
+                // Put name and phone number into Parse data table
+                ParseObject inputObject = new ParseObject("InputObject");
+                inputObject.put("Name", nameString);
+                inputObject.put("Phone Number", phoneString);
+                inputObject.saveInBackground();
 
-        if (ab != null) {
-            ab.setTitle(R.string.app_name);
-            ab.setLogo(R.drawable.ic_launcher);
-            ab.setDisplayUseLogoEnabled(true);
-            ab.setHomeButtonEnabled(enabled);
-        }
+                // Move to next Activity to test it
+                Intent i = new Intent(StartingActivity.this, ContactsActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
@@ -65,31 +78,6 @@ public class StartingActivity extends ActionBarActivity implements View.OnClickL
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-
-        if (id == R.id.enterButton) {
-            // Find EditText views
-            name = (EditText) findViewById(R.id.nameText);
-            phoneNumber = (EditText) findViewById(R.id.phoneNumberText);
-
-            // Get user input from EditText and add values to strings
-            nameString = name.getText().toString();
-            phoneString = phoneNumber.getText().toString();
-
-            // Put name and phone number into Parse data table
-            ParseObject inputObject = new ParseObject("InputObject");
-            inputObject.put("Name", nameString);
-            inputObject.put("Phone Number", phoneString);
-            inputObject.saveInBackground();
-
-            // Move to next Activity to test it
-            Intent i = new Intent(StartingActivity.this, ContactsActivity.class);
-            startActivity(i);
-        }
     }
 
     private boolean checkPlayServices() {
