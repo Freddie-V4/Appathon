5b30b11e5f8902fa40efdc4371acf11da94e4298
Freddie <fv4.programmer@gmail.com>
2015-04-02 22:36:20 -0400

    Get EditText input and put into Parse

--- app/src/main/java/appathon/bu/com/appathon/StartingActivity.java
+++ app/src/main/java/appathon/bu/com/appathon/StartingActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 /**
  * Created by FreddieV4 on 1/31/2015.
@@ -19,6 +21,10 @@ public class StartingActivity extends ActionBarActivity implements View.OnClickL
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private boolean enabled = true;
     private Button enter;
+    private EditText name;
+    private EditText phoneNumber;
+    private String nameString;
+    private String phoneString;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,7 +72,21 @@ public class StartingActivity extends ActionBarActivity implements View.OnClickL
         int id = v.getId();
 
         if (id == R.id.enterButton) {
+            // Find EditText views
+            name = (EditText) findViewById(R.id.nameText);
+            phoneNumber = (EditText) findViewById(R.id.phoneNumberText);
 
+            // Get user input from EditText and add values to strings
+            nameString = name.getText().toString();
+            phoneString = phoneNumber.getText().toString();
+
+            // Put name and phone number into Parse data table
+            ParseObject inputObject = new ParseObject("InputObject");
+            inputObject.put("Name", nameString);
+            inputObject.put("Phone Number", phoneString);
+            inputObject.saveInBackground();
+
+            // Move to next Activity to test it
             Intent i = new Intent(StartingActivity.this, ContactsActivity.class);
             startActivity(i);
         }
